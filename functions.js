308910c61e5b92e9ec07cfdb5c6ad9950757fa93
AvelineUK <drj1988uk@gmail.com>
2025-09-18 03:28:24 +0100

    .

--- functions.js
+++
@@ -1,9 +0,0 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.classList.add('active')
-}
-
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.classList.remove('active')
-}
