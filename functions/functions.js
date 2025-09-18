e12c92bcb16a7282be4f2e9f6ecb2401ff4f93e1
AvelineUK <drj1988uk@gmail.com>
2025-09-18 06:01:59 +0100

    .

--- functions/functions.js
+++ functions/functions.js
@@ -1,9 +1,18 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.classList.add('active')
+const sidebar = document.querySelector('.sidebar');
+const navbar = document.querySelector('.logo-navigation');
+
+function showSidebar() {
+    sidebar.classList.add('active');
 }
 
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.classList.remove('active')
+function hideSidebar() {
+    sidebar.classList.remove('active');
 }
+
+// Hide sidebar on desktop resize
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        sidebar.classList.remove('active');
+        navbar.style.transform = 'translateY(0)';
+    }
+})
