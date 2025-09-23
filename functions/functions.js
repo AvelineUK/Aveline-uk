0eab2818162402ec1dcdf2f886287cc2a129d6c3
AvelineUK <drj1988uk@gmail.com>
2025-09-23 05:10:18 +0100

    .

--- functions/functions.js
+++ functions/functions.js
@@ -92,3 +92,17 @@ window.addEventListener('DOMContentLoaded', () => {
                 });
             }
         });
+
+/* Scroll-triggered Animation */
+
+        const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        }
+    });
+});
+
+document.querySelectorAll('.fade-in').forEach(el => {
+    observer.observe(el);
+});
