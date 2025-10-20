be32cab8bc6ad1fcd0e72d37cd8dc8c6793873cf
AvelineUK <drj1988uk@gmail.com>
2025-10-20 17:45:12 +0100

    .

--- demo-2/functions.js
+++ demo-2/functions.js
@@ -116,7 +116,3 @@ document.addEventListener('click', function(e) {
         menu.classList.remove('active');
     }
 });
-
-document.getElementById('hero-video').addEventListener('loadeddata', function() {
-    this.classList.add('loaded');
-});
